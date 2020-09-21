ff949e98eb4714ed9b100fcf71426a3af3ef3d8f
cw chen <sky85245630@gmail.com>
2020-09-21 22:44:09 +0800

    request value

--- createServer.js
+++ createServer.js
@@ -1,15 +1,16 @@
 var http = require('http');
 
 http.createServer(function(request,response){
+    console.log(request)
     //表頭寫入200代表成功，後面的是傳送文字給server
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    //寫入的文字
-    response.write('hello world!!')
+    // response.writeHead(200,{"Content-Type":"text/plain"});
+    // //寫入的文字
+    // response.write('hello world!!')
 
 
     //若傳送html為text/html
-    // response.writeHead(200,{"Content-Type":"text/html"});
-    // response.write('<h1>hello world!!</h1>')
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write('<h1>hello world!!</h1>')
     //結束
     response.end();
 }).listen(8080);
